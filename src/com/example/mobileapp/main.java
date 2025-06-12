5e0b836600e0a139664e2618cf68ee00b8513474
amar714 <amrkulkarni10@gmail.com>
2025-06-12 12:18:14 +0530

    Stories/sprint 5/1234 contact service (#1)
    * Made the change from EC
    
    * Changes made from EC
    
    * Changes made
    
    ---------
    
    Co-authored-by: DELL <DELL@DESKTOP-6OKUOPN>

---
+++ src/com/example/mobileapp/main.java
@@ -0,0 +1,20 @@
+package com.example.mobileapp;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+   System.out.println("Hello World");
+   System.out.println("testing");
+   System.out.println("testing 3");
+   System.out.println("Adding code from ec");
+   
+   
+	} 
+}
