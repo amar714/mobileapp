64f55d1fe4775aceaea61e21abadc3588a57712f
Amar kulkarni <amrkulkarni10@gmail.com>
2025-06-12 11:56:31 +0530

    Changes made from EC

--- src/com/example/mobileapp/main.java
+++ src/com/example/mobileapp/main.java
@@ -9,6 +9,12 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-
+	
+   System.out.println("Hello World");
+   System.out.println("testing");
+   System.out.println("testing 3");
+   System.out.println("Adding code from ec");
+   
+   
+	} 
 }
